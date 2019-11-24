538f4798375a362976b6f2c59968bd160d486c04
Thamir Qadah <qadah.thamir@gmail.com>
2019-11-24 17:37:38 -0500

    refactored nav scroll code

--- js/main.js
+++ js/main.js
@@ -28583,6 +28583,37 @@ module.exports = function(Chart) {
 
 				return false;
 			};
+
+			if (jHeader.hasClass('is-expanded')){
+				jMenuToggler.click();
+			}
+
+			$("#top-bar__navigation li").removeClass('active');
+			$(this.parentElement).addClass('active');
+
+			// Make sure this.hash has a value before overriding default behavior
+			if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
+
+				// Store hash
+				var hash = this.hash;
+
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+				var stop = $(hash).offset().top;
+				console.log(stop);
+				if (stop > 0){
+					stop -= 57;
+				}
+				$('body,html').animate({
+					scrollTop: stop
+				}, 1200, function(){
+
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+				});
+			}  // End if
 		});
 
 		jMenuToggler.on('touchend click', function (e) {
@@ -29548,6 +29579,8 @@ module.exports = function(Chart) {
 		// smartresize 
 		jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 	})(jQuery,'smartresize');
+
+
 
 
 }(jQuery));
