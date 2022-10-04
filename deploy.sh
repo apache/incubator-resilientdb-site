0cd30b1a6c77f137b5d0d68a304c65da5146e33f
singhdivjeet <divjeet1@gmail.com>
2022-10-04 00:28:29 -0700

    deploy.sh fixed

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'resilientdb.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:nexres/nexres.github.io.git 
+git push -f git@github.com:resilientdb/resilientdb.github.io.git master:nexres-gh-pages
 cd -
