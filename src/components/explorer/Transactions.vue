e810fb28c66c10008addeaa1fdaac7a5ce4398b2
Apratim Shukla <apratimshukla6@gmail.com>
2023-11-03 12:16:12 -0700

    Update Transactions.vue

--- src/components/explorer/Transactions.vue
+++ src/components/explorer/Transactions.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { useBlocksStore } from "@/store/blocks";
 	import { storeToRefs } from "pinia";
-	import blocks from "../../api/transactions.json";
 	import { DownOutlined, FireTwoTone } from "@ant-design/icons-vue";
 	import { defineComponent } from "vue";
 	import { useRoute } from "vue-router";
@@ -48,9 +47,21 @@
 			const { refreshBlocks } = blocksStore;
 			await refreshBlocks();
 			
-			const block = blocks.value.filter(
+			let block = blocks.value.filter(
 				(b) => b.id === parseInt(route.query.id as string)
 			);
+
+			// Format transaction value JSON
+			for (let i = 0; i < block[0].transactions.length; i++) {
+				if (block[0].transactions[i].value) {
+					let value_str = String(block[0].transactions[i].value);
+					let value = JSON.parse(value_str);
+					block[0].transactions[i].value = JSON.stringify(value, null, 4);;
+				}
+				else {
+					block[0].transactions[i].value = "";
+				}
+			}
 			
 			return {
 				data: block[0].transactions,
@@ -63,9 +74,9 @@
 <template>
 	<div class="container timeline">
 		<div class="grid letOverflow">
-			<a-table :columns="columns" :data-source="data" >
+			<pre class="textWrapper"><a-table :columns="columns" :data-source="data" >
 				<template #title><strong>Transactions</strong> for Block <a :href="'/block?id=' + blockInfo[0].id">{{blockInfo[0].number}}</a></template>
-			</a-table>
+			</a-table></pre>
 		</div>
 	</div>
 </template>
@@ -84,6 +95,9 @@
 	.grid {
 		margin: 0 1rem;
 		padding: 0 1rem;
+	}
+	.textWrapper {
+		font-family: Poppins, 'Helvetica Neue', sans-serif;
 	}
 	
 </style>
