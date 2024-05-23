8a991c9859ff53ac4f1805900f22793f0c7dad86
Apratim Shukla <apratimshukla6@gmail.com>
2024-05-22 23:26:09 -0700

    Updated CSS

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section className="py-16 md:py-20 lg:py-28" style={{ backgroundImage: `url('data:image/svg+xml,<svg id="visual" viewBox="0 0 2000 1000" width="2000" height="1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="2000" height="1000" fill="%230F0F1A"></rect><g transform="translate(961.35041104432 476.3922111923553)"><path d="M284 -215.3C365.2 -124.6 426.4 -6.7 407.5 99.8C388.6 206.3 289.8 301.4 172.6 357.3C55.5 413.2 -79.8 430.1 -175.8 378.6C-271.8 327.2 -328.5 207.6 -333.3 100.6C-338.2 -6.4 -291.3 -100.6 -227.1 -187.4C-162.9 -274.2 -81.4 -353.6 10 -361.6C101.4 -369.5 202.8 -306 284 -215.3" fill="%23151523"></path></g></svg>')`,backgroundSize: 'cover', backgroundPosition: 'center bottom' }}>
+    <section className="py-16 md:py-20 lg:py-28" style={{ backgroundImage: `url('data:image/svg+xml,<svg id="visual" viewBox="0 0 2000 1000" width="2000" height="1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="2000" height="1000" fill="%230F0F1A"></rect><g transform="translate(961.35041104432 476.3922111923553)"><path d="M284 -215.3C365.2 -124.6 426.4 -6.7 407.5 99.8C388.6 206.3 289.8 301.4 172.6 357.3C55.5 413.2 -79.8 430.1 -175.8 378.6C-271.8 327.2 -328.5 207.6 -333.3 100.6C-338.2 -6.4 -291.3 -100.6 -227.1 -187.4C-162.9 -274.2 -81.4 -353.6 10 -361.6C101.4 -369.5 202.8 -306 284 -215.3" fill="%23151523"></path></g></svg>')`, backgroundSize: 'cover', backgroundPosition: 'center bottom' }}>
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
