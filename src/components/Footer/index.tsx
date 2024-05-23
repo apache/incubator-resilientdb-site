d1159fe1c1de1b1a86027588edc013885ee63279
Apratim Shukla <apratimshukla6@gmail.com>
2024-05-23 15:16:26 -0700

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -83,7 +83,7 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      href="https://resilientdb.expolab.org"
+                      href="https://expolab.resilientdb.com"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Learn More
