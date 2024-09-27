d904afccb4fd6862209f2167d70a9ac388b5046c
Apratim Shukla <apratimshukla6@gmail.com>
2024-09-27 01:38:03 -0700

    Update index.tsx

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -29,7 +29,7 @@ const Download = () => {
       <div className="container">
         <SectionTitle
           title="Download"
-          paragraph="Download Non-ASF release of Apache ResilientDB (Incubating)"
+          paragraph="Download ASF release of Apache ResilientDB (Incubating)"
           center
           width="665px"
         />
