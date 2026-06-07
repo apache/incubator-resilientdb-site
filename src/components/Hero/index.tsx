541afc296abec9cb567637c6f4882fc4fb93bd10
Bismanpal Singh <bisman.singh24@gmail.com>
2026-06-07 13:09:25 -0700

    Fixed hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import { faBook, faRobot } from '@fortawesome/free-solid-svg-icons';
 
 const Hero = () => {
@@ -21,11 +22,17 @@ const Hero = () => {
                 Apache ResilientDB (Incubating) offers a high-throughput yielding distributed ledger built upon scale-centric design principles to democratize and decentralize computation
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-                <Link
+                  {/* <Link
                     href="https://youtu.be/QlwnZuBHqM4"
                     className="flex items-center justify-center rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80 w-40 h-15"
                   >
                   <FontAwesomeIcon icon={faPlayCircle} className="text-base mr-2 h-5" /> Vision
+                  </Link> */}
+                  <Link
+                    href="https://github.com/apache/incubator-resilientdb"
+                    className="flex items-center justify-center rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80 w-40 h-15"
+                  >
+                  <FontAwesomeIcon icon={faGithub} className="text-base mr-2 h-5" /> Codebase
                   </Link>
                   <Link
                     href="https://resai-hub.resilientdb.com/"
